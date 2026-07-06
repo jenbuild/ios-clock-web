3b50b584035e0cce6bd2570985592045275d5128
Ajay Kumar <iajaymk+github@gmail.com>
2026-07-07 05:12:39 +0530

    code refactor

--- lib/world-clock.ts
+++ lib/world-clock.ts
@@ -1,6 +1,10 @@
-export function getCityKey(city: {
-    city: string;
-    country: string;
-}) {
-    return `${city.city}-${city.country}`;
+import { WorldClock } from "@/types/worldclock";
+
+export function getCityKey(city: WorldClock) {
+	return `${city.city}-${city.country}`
+		.normalize("NFD")
+		.replace(/[\u0300-\u036f]/g, "")
+		.replace(/[^a-zA-Z0-9]+/g, "-")
+		.replace(/^-|-$/g, "")
+		.toLowerCase();
 }
